10ee4847a9c112fe0ebee3761b73918080ac177b
Dhruv-158 <dhruvprajapati152@gmail.com>
2024-10-16 15:33:19 +0530

    all done things

--- Blog-Project/src/components/index.js
+++ Blog-Project/src/components/index.js
@@ -3,8 +3,11 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-
+import Button from "./Button";
+import Input from "./Input";
+import Postcard from "./Postcard";
+import Select from "./Select";
 
 export {
-    Header,Footer,Container,Logo,LogoutBtn
+    Header,Footer,Container,Logo,LogoutBtn,Button,Input,Postcard,Select
 }
